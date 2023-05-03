a68e7246ea2b6c6358797ebafdf0c0b372d575d6
Vlad <savenkov.vla@yandex.ru>
2023-05-03 22:49:56 +0700

    added 01_types.js

--- 01_types.js
+++ 01_types.js
@@ -1,1 +1,16 @@
-console.log('123')
+// в JS 7 типов данных
+// null, undefined, boolean string, number, object(единственный не примитивный тип данных), symbol(ES6+)
+
+//------
+// неточности оператора typeof
+// console.log(typeof null) // object // тип данных null не object, а null
+// console.log(typeof function () {}) // function // тип данных function не function, а onject
+// console.log(typeof NaN) // number // тип данных not a number действительно number
+
+//------
+// отличие null и undefined
+// // null является определенным значением отсутствия объекта, а undefined обозначает неопределенность
+// let element
+// console.log(element) // undefined, так как значение не определенно
+// element = document.getElementById('element')
+// console.log(element) // null, так как значение определенно, но элемента с id element не существует, поэтому getElementById вернет null
